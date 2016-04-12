b047296f5269fad590b69f002c7c46fe6e8968a9
Mollie <mollie@civil.cm>
2016-04-11 19:51:23 -0700

    fix minor spelling error

--- js/getRepos.js
+++ js/getRepos.js
@@ -10,7 +10,7 @@ exports.getUserData = function(username){
 };
 
 exports.getRepos = function(username){
-    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + githubKey).then(function(response){
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
         console.log(response);
         for(var i = 0; i < response.length; i++) {
             $('#repoList').append('<li>Name: ' + response[i].name + '</li>');
