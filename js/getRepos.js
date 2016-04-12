0e55f999e7b5d215c282795068b696dd1250ed14
Mollie <mollie@civil.cm>
2016-04-11 19:49:48 -0700

    split out buttons to perform different functions

--- js/getRepos.js
+++ js/getRepos.js
@@ -1,15 +1,20 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(username){
+exports.getUserData = function(username){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    $('#output').prepend("<div class='user_info'><img class='user_img' src='" + response.avatar_url + "'><h2 class='user_name'>" + response.login + "</h2><h4>" + response.location + "</h4><p><strong>Followers:</strong> " + response.followers + "</p><p><strong>Following:</strong> " + response.following + "</p><p><strong>Account Created:</strong> " + response.created_at + "</p><a class='repos' href='" + response.repos_url + "'>List of Public Repos");
+    $('#output').prepend("<div class='user_info'><img class='user_img' src='" + response.avatar_url + "'><h2 class='user_name'>" + response.login + "</h2><h4>" + response.location + "</h4><p><strong>Followers:</strong> " + response.followers + "</p><p><strong>Following:</strong> " + response.following + "</p><p><strong>Account Created:</strong> " + response.created_at + "</p><button class='btn getRepos' type='button'>Get User Repos</button>");
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
 
-// exports.getRepoList = function() {
-//     $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-//       $('#user_info').append("");
-// };
+exports.getRepos = function(username){
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + githubKey).then(function(response){
+        console.log(response);
+        for(var i = 0; i < response.length; i++) {
+            $('#repoList').append('<li>Name: ' + response[i].name + '</li>');
+            $('#repoList').append('<li>Link: <a href="' + response[i].html_url + '">' + response[i].html_url + '</li><br>');
+        }
+    });
+};
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,8 +1,15 @@
 var apiKey = require('./../.env').apiKey;
 var getRepos = require('./../js/getRepos.js').getRepos;
+var getUserData = require('./../js/getRepos.js').getUserData;
 
 $(document).ready(function() {
-  $('#search').click(function() {
+  $('#search-profile').click(function() {
+    var username = $('#username').val();
+    $('#username').val("");
+    getUserData(username);
+  });
+
+  $('#search-repos').click(function() {
     var username = $('#username').val();
     $('#username').val("");
     console.log(username);
